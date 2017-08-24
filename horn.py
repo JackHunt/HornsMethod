9cf57e28931ef3c24e1a17ade9ec6d5b385474fc
Jack Hunt <jackmileshunt@gmail.com>
2017-08-24 19:31:32 +0100

    bug fix

--- horn.py
+++ horn.py
@@ -7,17 +7,15 @@ import mayavi.mlab as mlab
 def generateRandomPoints(n, min, max):
         return np.random.uniform(min, max, (n, 3))
         
-def applyRandomTransform(points, rotSigma, transMax, noiseSD):
+def applyRandomTransform(points, rangeRad, transMax, noiseSD):
         #Generate random Euler Angles(I think.. there must be a better way to do this)
         #Seems to work for this demo however.
-        variance = rotSigma*rotSigma
-        covariance = np.eye(3) * variance
-        alpha, beta, gamma = np.random.multivariate_normal(np.zeros(3), covariance)
+        alpha, beta, gamma = np.random.uniform(0, rangeRad, 3)
 
         #Build rotation matrices for each principal axis.
         Rx = np.matrix([[np.cos(alpha), -np.sin(alpha), 0], [np.sin(alpha), np.cos(alpha), 0], [0, 0, 1]])
-        Ry = np.matrix([[np.cos(beta), -np.sin(beta), 0], [np.sin(beta), np.cos(beta), 0], [0, 0, 1]])
-        Rz = np.matrix([[np.cos(gamma), -np.sin(gamma), 0], [np.sin(gamma), np.cos(gamma), 0], [0, 0, 1]])
+        Ry = np.matrix([[np.cos(beta), 0, np.sin(beta)], [0, 1, 0], [-np.sin(beta), 0, np.cos(beta)]])
+        Rz = np.matrix([[1, 0, 0], [0, np.cos(gamma), -np.sin(gamma)], [0, np.sin(gamma), np.cos(gamma)]])
         
         #Get random translation vector.
         t = np.random.uniform(1, 10, (1, 3))
@@ -68,10 +66,10 @@ def displayPoints(pointsA, pointsB, pointsARectified):
         plotPointPair("AfterTransform", pointsARectified, pointsB)
         mlab.show()
 
-def runDemo(numPoints, bcMin, bcMax, sdRad, transMax, noiseSD):
+def runDemo(numPoints, bcMin, bcMax, rangeRad, transMax, noiseSD):
         #Generate random points and apply random rigid transform.
         pointsA = generateRandomPoints(numPoints, bcMin, bcMax)
-        transformed = applyRandomTransform(pointsA, sdRad, transMax, noiseSD)
+        transformed = applyRandomTransform(pointsA, rangeRad, transMax, noiseSD)
 
         #Apply Horns Method to align the point clouds.
         trans = horn(pointsA.transpose(), transformed[0].transpose())
@@ -82,8 +80,11 @@ def runDemo(numPoints, bcMin, bcMax, sdRad, transMax, noiseSD):
 
         #Calculate RMS.
         diff = transformed[0] - rectifiedPoints
-        rms = np.sqrt((diff**2).mean())
-        print("Registration RMS: %f" % rms)
+        rmse = np.sqrt((diff**2).mean())
+
+        #Give output.
+        np.set_printoptions(formatter={'float_kind':'{:f}'.format})
+        print("Registration RMSE: %f" % rmse)
         print("True rotation:")
         print(transformed[5])
         print("True translation:")
@@ -98,13 +99,13 @@ if __name__ == "__main__":
         argParser.add_argument('num_points', help="Number of test points to generate.")
         argParser.add_argument('bc_min', help="Min x,y,z value for genrated points(within bounding cube).")
         argParser.add_argument('bc_max', help="Max x,y,z value for genrated points(within bounding cube).")
-        argParser.add_argument('--sd_radians', help="Standard Deviation of randomised Euler Angles. [0, 2pi)")
+        argParser.add_argument('--range_radians', help="Range of randomised Euler Angles. [0, 2pi)")
         argParser.add_argument('--trans_max', help="Max value for random translation.")
         argParser.add_argument('--noise_sd', help="Random noise Standard Deviation for transformed points.")
         args = argParser.parse_args()
 
-        sdRad = float(args.sd_radians) if args.sd_radians else 2*np.pi
+        rangeRad = float(args.range_radians) if args.range_radians else 2*np.pi
         transMax = int(args.trans_max) if args.trans_max else 10
         noiseSD = float(args.noise_sd) if args.noise_sd else None
 
-        runDemo(int(args.num_points), int(args.bc_min), int(args.bc_max), sdRad, transMax, noiseSD)
+        runDemo(int(args.num_points), float(args.bc_min), float(args.bc_max), rangeRad, transMax, noiseSD)
